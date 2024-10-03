8b65a77689610f8e6c50da85ed51c61ddded3ad8
Harsh Badhai <harshbadhai242@gmail.com>
2024-10-04 00:08:25 +0530

    small ui fix

--- src/app/page.js
+++ src/app/page.js
@@ -70,7 +70,7 @@ export default function Home() {
                   <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
                   <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
                 </span>
-                Live on Aptos Devnet
+                Live on Aptos Testnet
               </Link>
             </div>
           </div>
