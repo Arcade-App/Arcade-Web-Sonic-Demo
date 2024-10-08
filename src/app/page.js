24974aef2fc5b23e1edb214b67aceb5026844f8d
Harsh Badhai <harshbadhai242@gmail.com>
2024-10-08 22:55:16 +0530

    devnet

--- src/app/page.js
+++ src/app/page.js
@@ -69,7 +69,7 @@ export default function Home() {
                   <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
                   <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
                 </span>
-                Live on Solana Testnet
+                Live on Solana Devnet
               </Link>
             </div>
           </div>
