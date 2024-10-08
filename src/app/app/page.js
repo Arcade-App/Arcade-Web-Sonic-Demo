d4a44eed1baf7d8d4c9258efc46a8b7bf72c89eb
Harsh Badhai <harshbadhai242@gmail.com>
2024-10-09 00:14:29 +0530

    user select disabled

--- src/app/app/page.js
+++ src/app/app/page.js
@@ -26,6 +26,7 @@ export default function Game() {
                         "none", position: "absolute", top: 0, left: 0, right: 0, bottom: 0
                 }}
                 allowFullScreen
+                className="select-none"
             >
             </iframe>
         </div>
